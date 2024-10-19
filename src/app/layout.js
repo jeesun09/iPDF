4996eefdb90af6dae777ddd4448f9099d81154b3
Jeesun Bari <jeesunbari2002@gmail.com>
2024-10-19 10:13:06 +0000

    Refactor code: Update metadata in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,49 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "iPDF",
+  metadataBase: new URL("https://ipdf-jeesun.vercel.app/"),
+  title: "iPDF - AI-Driven PDF Interaction",
   description:
     "Unlock the power of your PDFs with iPDF - an AI-driven platform that lets you interact with your documents effortlessly. Get insights and answers from your PDFs like never before.",
+  authors: [
+    {name: "Jeesun Bari", url: 'https://www.jeesunbari.online/'}
+  ],
+  openGraph: {
+    title: "iPDF - AI-Driven PDF Interaction",
+    description:
+      "Effortlessly interact with your PDFs using AI. Extract insights and get answers from documents like never before.",
+    type: "website",
+    url: "https://ipdf-jeesun.vercel.app/",
+    images: [
+      {
+        url: "/images/favicon.svg",
+        width: 800,
+        height: 600,
+        alt: "iPDF - AI-Driven PDF Interaction",
+      },
+    ],
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "iPDF - AI-Driven PDF Interaction",
+    description:
+      "Effortlessly interact with your PDFs using AI. Extract insights and get answers from documents like never before.",
+    creator: "@JeesunSk",
+    images: [
+      {
+        url: "/images/favicon.svg",
+        alt: "iPDF - AI-Driven PDF Interaction",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+  appleWebApp: {
+    capable: true,
+    title: "iPDF - AI-Driven PDF Interaction",
+    statusBarStyle: "black-translucent",
+  },
 };
 
 export default function RootLayout({ children }) {
